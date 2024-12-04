9f345cf3a81d2cff0c7cdbf8b04d5f7c46182b2d
ZPannatlertP <pannatlert.zen@gmail.com>
2024-12-04 11:05:56 +0700

    Lab103

--- LabSheet01/src/Lab103.java
+++ LabSheet01/src/Lab103.java
@@ -1,27 +1,17 @@
-import java.util.*;
-
-import javax.swing.JOptionPane;
-
-import java.text.*;
-import java.text.DecimalFormat;
-import java.text.DecimalFormat;
+import javax.swing.*;
 public class Lab103 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		DecimalFormat frm = new DecimalFormat("#,###.00");
 
 		int timeparking = Integer.parseInt(JOptionPane.showInputDialog("Input Time parking (minute): "));
 		
 		int hour = timeparking / 60;
 		int min = timeparking % 60;
-
 		
 		double price= (hour * 50)+(min * 0.25);
 
-		String message = String.format("Your Parking %d Hour "" Minute."
-											+ "\nAmount to be paid is "" baht.");
-
+		String message = String.format("Your Parking %d Hour %d Minute.\nAmount to be paid is %,.1f baht.",hour,min,price);
+		JOptionPane.showMessageDialog(null, message);
 
 	}
 
