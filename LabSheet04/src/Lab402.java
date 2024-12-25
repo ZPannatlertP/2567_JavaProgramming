520c8042186b5a35a083bf86fb22a1fe4abe8536
ZPannatlertP <pannatlert.zen@gmail.com>
2024-12-25 10:20:18 +0700

    LabSheet4 example

---
+++ LabSheet04/src/Lab402.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+public class Lab402 {
+ 
+public static void main(String[] args)
+{
+	int year = Integer.parseInt(JOptionPane.showInputDialog(null,"Input year: "));
+	
+	while (checkYear(year)==false)
+	{
+		year = Integer.parseInt(JOptionPane.showInputDialog(null,"Input year: "));
+	}
+	
+	if(isLeapYear(year)==true) {
+		JOptionPane.showMessageDialog(null,year+ "is Leap year.");
+	}
+	else {
+		JOptionPane.showMessageDialog(null,year+ "is NOT Leap year.");
+	}
+ 
+}//end of main
+
+public  static boolean isLeapYear (int _year) {
+	if(_year%4==0 && _year%100!=0 || _year%400==0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+public static boolean checkYear(int _year) {
+	if (_year>=1000&&_year<=3000) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+ 
+}
