7103a45da07e6a35361af1dfb9ca183cfcefa5c2
ZPannatlertP <pannatlert.zen@gmail.com>
2025-01-29 11:28:29 +0700

    Lab8 Pratice

--- Labsheet8/src/Product.java
+++ Labsheet8/src/Product.java
@@ -1,5 +1,29 @@
 
 public class Product {
 	private String productid;
-	private int studentScore;
+	private int productUnit;
+	private double productPrice;
+	
+	public void setId(String pid) {
+		this.productid=pid;
+	}
+	public String getId() {
+		return this.productid;
+	}
+	public void setUnit(int proUnit) {
+		this.productUnit=proUnit;
+	}
+	public int getUnit() {
+		return this.productUnit;
+	}
+	public void setPrice(double proPrice) {
+		this.productPrice=proPrice;
+	}
+	public double getPrice() {
+		return this.productPrice;
+	}
+	public double calculate() {
+		return productUnit * productPrice;
+	}
+	
 }
--- Labsheet8/src/StockProduct.java
+++ Labsheet8/src/StockProduct.java
@@ -1,4 +1,33 @@
-
+import java.util.*;
 public class StockProduct {
 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Product[] pid = new Product[4];
+        for (int i = 0; i < pid.length; i++) {
+            pid[i] = new Product();
+            System.out.print("Input product Id    :");
+            pid[i].setId(scan.nextLine());
+            System.out.print("Input product Unit  : ");
+            int proUnit = scan.nextInt();
+            scan.nextLine();
+            System.out.print("Input product Price : ");
+            Double proPrice = scan.nextDouble();
+            scan.nextLine();
+            System.out.println();
+
+        }
+        Line();
+        for(Product Pid : pid) {
+        	System.out.println("Product ID : " + Pid.getId()+", Total price = "+Pid.calculate());
+        }
+        Line();
+        
+    }
+    public static void Line() {
+        for (int i = 1; i < 50; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
 }
